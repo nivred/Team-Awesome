c5c2d5e88b2ddc93883b9da7fbc2b43b056a5ba1
Laura A. Ward <law446@ads.northwestern.edu>
2018-02-17 15:34:00 -0600

    adding server.js

--- reactTemp/brainDead/server.js
+++ reactTemp/brainDead/server.js
@@ -1,10 +1,28 @@
-const express = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
-const app = express();
-const port = process.env.PORT || 5000;
+//test
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
+var app = express();
+var port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/index.js");
+
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("app listening on port", PORT)
+});
