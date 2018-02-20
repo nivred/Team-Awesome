b41d2dce0a9a4e2020c826fd0bf085a6c5eddb01
nivred <emaildervin@gmail.com>
2018-02-19 20:56:54 -0600

    successfully displying 12 cards with images on screen

--- reactTemp/brainDead/client/src/Components/ClickItem/ClickItem.js
+++ reactTemp/brainDead/client/src/Components/ClickItem/ClickItem.js
@@ -7,7 +7,7 @@ const ClickItem = props => (
     aria-label="click item"
     onClick={() => props.clickHandler(props.id)}
     style={{ backgroundImage: `url("${props.image}")` }}
-    className={`click-item${props.shake ? " shake" : ""} pull-left`}
+    className={`click-item${props.shake ? " shake" : ""} col-md-2 pull-left`}
   />
 );
 
--- reactTemp/brainDead/client/src/Components/Game.js
+++ reactTemp/brainDead/client/src/Components/Game.js
@@ -20,16 +20,18 @@ class Game extends Component {
         return (
             <div className="wrapper">
                 <div className="container-fluid text-center">    
-                    <div id="main-content" className="row content col-md-3">
-                        {this.state.ShuffleDeck.map(item => (
-                            <ClickItem
-                                key={item.id}
-                                id={item.id}
-                                shake={!this.state.score && this.state.topScore}
-                                handleClick={this.handleItemClick}
-                                image={item.image}
-                            />
-                        ))}
+                    <div id="main-content" className="row content">
+                        <div class="col-xs-12">
+                            {this.state.ShuffleDeck.map(item => (
+                                <ClickItem
+                                    key={item.id}
+                                    id={item.id}
+                                    shake={!this.state.score && this.state.topScore}
+                                    handleClick={this.handleItemClick}
+                                    image={item.image}
+                                />
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
