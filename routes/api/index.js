9ead60143ecb5f98aed543db653af96a3536214f
LauraAWard <l-ward@northwestern.edu>
2018-03-04 20:16:38 -0600

    cleaned up route files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,18 +1,11 @@
 const path = require("path");
 const express = require("express");
-// const fs = require('fs');
 
 const router = express.Router();
-// const userRoutes = require("./users");
-// const scoreRoutes = require('./score');
-// const gameRoutes = require('./game');
 
 const userController = require("../../controllers/userController");
 const scoreRoutes = require('../../controllers/scoreController');
 const gameRoutes = require('../../controllers/gameController');
-
-
-console.log("hello from routes/api/index");
 
 // routes
 router.use("/user", userController);
