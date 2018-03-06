f7ebc1834e28c06f7c6927b7b1a541b933d8aa9a
LauraAWard <l-ward@northwestern.edu>
2018-03-06 17:28:30 -0600

    added logout to stats page

--- client/src/Components/Logout.js
+++ client/src/Components/Logout.js
@@ -1,31 +1,38 @@
  import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { matchPath } from 'react-router';
 import '../App.css';
 
-export default class Logout extends Component {  
+class Logout extends Component {  
 
-    render() {
+state = {
+
+   pathname: this.props.pathname 
+}
+
+
+    render() {   
+       
         return (
-            <div>
-                <div id="controller" className="col-md-4 text-center">
-                    {/* <a href="#" type="button" className="btn btn-success" onClick={this.Start}><span className="glyphicon glyphicon-play-circle "></span></a> */}
-                    <span id="timer"><span id="minutes">00</span>:<span id="seconds">00</span></span>
-                    {/* <a href="#" className="btn btn-danger dropdown-toggle" data-toggle="dropdown"><span className="glyphicon glyphicon-cog"></span></a>
-                    <ul className="dropdown-menu" role="menu" aria-labelledby="menu1">
-                        <li role="presentation" className="dropdown-header">Display Time</li>
-                        <li role="presentation"><a role="menuitem" tabIndex="-1" href="#">1 sec</a></li>
-                        <li role="presentation"><a role="menuitem" tabIndex="-1" href="#">2 sec</a></li>
-                        <li role="presentation"><a role="menuitem" tabIndex="-1" href="#">3 sec</a></li>
-                        <li role="presentation"><a role="menuitem" tabIndex="-1" href="#">4 sec</a></li>    
-                    </ul> */}
+        <div className="wrapper">
+                <div className="container-fluid">
+                <div className="col-md-12" id="myNavbar">
+                    <ul className="nav navbar-nav navbar-right">
+                        { (this.state.pathname == "Game") ?
+                        <li><Link to="/Stats"><span className="glyphicon glyphicon-stats"></span> Stats</Link></li>
+                          : <li><Link to="/Game"><span className="glyphicon glyphicon-stats"></span> Game</Link></li> }
+                        <li><Link to="/"><span className="glyphicon glyphicon-log-out"></span> Sign out</Link></li> 
+                    </ul>               
                 </div>
-            </div>
+                </div>
+         </div>
         )
     }
 }
 
 
 
+export default Logout;
 
 
 
@@ -36,12 +43,3 @@ export default class Logout extends Component {
 
 
 
-
-
-
-                <div className="col-md-4" id="myNavbar">
-                    <ul className="nav navbar-nav navbar-right">
-                        <li><Link to="/Stats"><span className="glyphicon glyphicon-stats"></span> Stats</Link></li>
-                        <li><Link to="/"><span className="glyphicon glyphicon-log-out"></span> Sign out</Link></li>
-                    </ul>               
-                </div>
--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import { Switch, Route, BrowserRouter as Router, Redirect, Link } from 'react-router-dom';
 import { Switch, Route,Link} from 'react-router-dom';
 import Timer from "./Timer";
+import Logout from "./Logout";
 import '../App.css';
 // import data from "./data.json";
 
@@ -22,15 +23,22 @@ class Nav extends Component {
         <div className="wrapper">
             <nav className="navbar navbar-inverse">
                 <div className="container-fluid">
-                    <div className="navbar-header col-md-4">
+                    <div className="navbar-header col-md-5">
                         <div className="navbar-brand">
                             <Link to="/Game"><img src="../assets/images/brainDead.png" alt=""/></Link>
                         </div>
                     </div>
-
-                    <Switch>
-                        <Route exact path="/Game" component={() => <Timer {...this.props}/>} />
-                    </Switch>
+                    <div className="navbar-header col-md-2">
+                        <Switch>
+                            <Route exact path="/Game" component={() => <Timer {...this.props}/>} />
+                       </Switch>
+                    </div>
+                    <div className="navbar-header col-md-5">
+                        <Switch>
+                            <Route exact path="/Game" component={() => <Logout {...this.props} pathname="Game" />} />
+                            <Route exact path="/Stats" component={() => <Logout {...this.props} pathname="Stats" />} />
+                        </Switch>
+                    </div>
                 </div>
             </nav>
         </div>
