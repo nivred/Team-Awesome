a93682520cb201d14ba0c7fda974f6b649a7bd4c
LauraAWard <l-ward@northwestern.edu>
2018-03-06 23:18:56 -0600

    added glyph for game link

--- client/src/Components/Logout.js
+++ client/src/Components/Logout.js
@@ -20,7 +20,7 @@ state = {
                     <ul className="nav navbar-nav navbar-right">
                         { (this.state.pathname == "Game") ?
                         <li><Link to="/Stats"><span className="glyphicon glyphicon-stats"></span> Stats</Link></li>
-                          : <li><Link to="/Game"><span className="glyphicon glyphicon-stats"></span> Game</Link></li> }
+                          : <li><Link to="/Game"><span className="glyphicon glyphicon-th"></span> Game</Link></li> }
                         <li><Link to="/"><span className="glyphicon glyphicon-log-out"></span> Sign out</Link></li> 
                     </ul>               
                 </div>
