0d67ee10afb5fdae0f0bdd1c78a64a91a7ddeb89
LauraAWard <l-ward@northwestern.edu>
2018-03-06 21:35:47 -0600

    commented ORM

--- models/orm.js
+++ models/orm.js
@@ -3,6 +3,7 @@ const connection = require("../config/connection.js");
 
 //object containing functions for SQL queries
 const orm = {
+  //select all from table where condition
   select: function(table, condition, callback){
     let querystring = "SELECT * FROM ?? WHERE ? ";
     let parms = [table, condition];
@@ -13,7 +14,7 @@ const orm = {
       callback(result);
     })
   },
- 
+  //select all from table where condition1 or condition2
   selectOr: function(table, condition1, condition2, callback){
     let querystring = "SELECT * FROM ?? WHERE ? OR ?";
     let parms = [table, condition1, condition2];
@@ -24,7 +25,7 @@ const orm = {
       callback(result);
     })
   },
-
+  //select all from table where condition1 and condition2
   selectAnd: function(table, condition1, condition2, callback){
     let querystring = "SELECT * FROM ?? WHERE ? AND ?";
     let parms = [table, condition1, condition2];
@@ -35,7 +36,7 @@ const orm = {
       callback(result);
     })
   },
-
+  //select all from table where column has value like (partial string search)
   selectLike: function(table, col, value, callback){
     let querystring = "SELECT * FROM ?? WHERE ?? LIKE ?";
     let parms = [table, col, value];
@@ -46,7 +47,7 @@ const orm = {
       callback(result);
     })
   },
-  
+  //select all from table sorted by column ascending
   selectSortA: function(table, sortfield, callback){
     let querystring = "SELECT * FROM ?? ORDER BY ?? ASC";
     let parms = [table, sortfield];
@@ -57,7 +58,7 @@ const orm = {
       callback(result);
     })
   },
-
+  //select all from table sorted by column descending
   selectSortD: function(table, sortfield, callback){
     let querystring = "SELECT * FROM ?? ORDER BY ?? DESC";
     let parms = [table, sortfield];
@@ -68,7 +69,7 @@ const orm = {
       callback(result);
     })
   },
-
+  //select all from table where condition order by column ascending
   selectBySortA: function(table, condition, sortfield, callback){
     let querystring = "SELECT * FROM ?? WHERE ? ORDER BY ?? ASC";
     let parms = [table, condition, sortfield];
@@ -79,7 +80,7 @@ const orm = {
       callback(result);
     })
   },
-
+  //select all from table where condition order by column descending
   selectBySortD: function(table, condition, sortfield, callback){
     let querystring = "SELECT * FROM ?? WHERE ? ORDER BY ?? DESC";
     let parms = [table, condition, sortfield];
@@ -90,7 +91,7 @@ const orm = {
       callback(result);
     })
   },
-
+  //select all from table
   selectAll: function(table, callback) {
     let queryString = "SELECT * FROM ?? ";
     let parms = [table];
@@ -102,7 +103,7 @@ const orm = {
       callback(result);
     });
   },
-
+  //get rowcount from table where condition
   countBy: function(table, field, condition, callback){
     let querystring = "SELECT COUNT(??) FROM ?? WHERE ? ";
     let parms = [table, field, condition];
@@ -113,7 +114,7 @@ const orm = {
       callback(result);
     })
   },
-
+  //update table set values where condition
   update: function(table, value, condition, callback){
     let querystring = "UPDATE ?? SET ? WHERE ? ";
     let parms = [table, value, condition];
@@ -124,7 +125,7 @@ const orm = {
       callback(result);
     });
   },
-
+  //select all from table1, table2 join on column1, column2 where condition
   joinSelect: function(table1, table2, col1, col2, condition, callback){
     let querystring = "SELECT * FROM ??, ?? WHERE ?? = ?? AND ? ";
     let parms = [table1, table2, col1, col2, condition];
@@ -135,7 +136,7 @@ const orm = {
       callback(result);
     })
   },
-
+  //select all from table1, table2 join on column1, column2 order by column3 ascending
   joinSortA: function(table1, table2, col1, col2, sortfield, callback){
     let querystring = "SELECT * FROM ??, ?? WHERE ?? = ?? ORDER BY ?? ASC";
     let parms = [table1, table2, col1, col2, sortfield];
@@ -146,7 +147,7 @@ const orm = {
       callback(result);
     })
   },
-
+  //select distinct fields from table1, table2 join on column1, column2 order by column3 ascending
   distinctJoinSortA: function(fields, table1, table2, col1, col2, sortfield, callback){
     let querystring = "SELECT DISTINCT ?? FROM ??, ?? WHERE ?? = ?? ORDER BY ?? ASC";
     let parms = [fields, table1, table2, col1, col2, sortfield];
@@ -157,8 +158,7 @@ const orm = {
       callback(result);
     })
   },
-
-
+  //select all from table1, table2 join on column1, column2 order by column3 descending
   joinSortD: function(table1, table2, col1, col2, sortfield, callback){
     let querystring = "SELECT * FROM ??, ?? WHERE ?? = ?? ORDER BY ?? DESC";
     let parms = [table1, table2, col1, col2, sortfield];
@@ -169,7 +169,7 @@ const orm = {
       callback(result);
     })
   },
-
+  //select distinct fields from table1, table2 join on column1, column2 order by column3 descending
   distinctJoinSortD: function(fields, table1, table2, col1, col2, sortfield, callback){
     let querystring = "SELECT DISTINCT ?? FROM ??, ?? WHERE ?? = ?? ORDER BY ?? DESC";
     let parms = [fields, table1, table2, col1, col2, sortfield];
@@ -180,7 +180,7 @@ const orm = {
       callback(result);
     })
   },
-
+  //select all from table1, table2 join column1, column2 where condition order by sortfield ascending
   joinSelectSortA: function(table1, table2, col1, col2, condition, sortfield, callback){
     let querystring = "SELECT * FROM ??, ?? WHERE ?? = ?? AND ? ORDER BY ?? ASC";
     let parms = [table1, table2, col1, col2, condition, sortfield];
@@ -191,7 +191,7 @@ const orm = {
       callback(result);
     })
   },
-
+  //select all from table1, table2 join column1, column2 where condition order by sortfield descending
   joinSelectSortD: function(table1, table2, col1, col2, condition, sortfield, callback){
     let querystring = "SELECT * FROM ??, ?? WHERE ?? = ?? AND ? ORDER BY ?? DESC";
     let parms = [table1, table2, col1, col2, condition, sortfield];
@@ -202,7 +202,7 @@ const orm = {
       callback(result);
     })
   },
-
+  //insert into table columns -> values
   insertRow: function(table, dataObject, callback){
     let queryString = "INSERT INTO ?? (??) VALUES (?) ";
     let parms = [table];
