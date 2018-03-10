bc2a86945af25d20884ce19040565c80ea6a6a6e
stabrr <bobbystabile@yahoo.com>
2018-03-09 18:22:04 -0600

    Bryan Fixed Email

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -5,8 +5,7 @@ import '../App.css';
 import API from "../utils/API";
 const bcrypt = require("bcryptjs");
 
-const emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
+   const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 class Login extends Component {
     
   state = {
